c88cb4dc4cc78d9193172ed13a7444421b9a23a2
PolygonPrincess <116114153+PolygonPrincess@users.noreply.github.com>
2022-10-18 20:48:26 -0400

    Various patch modifications
    - Added more descriptive text to user about what the patch is for
    - Added prompts for user to confirm continuing the process
    - Added information about what versions are found and if the files are changed
    - Added visual changes to text to easily decipher between information
    - Modified the script to run as a loop instead of three separate if statements

--- dbd-120-fps.ps1
+++ dbd-120-fps.ps1
@@ -1,36 +1,30 @@
 $steam = "$($ENV:LOCALAPPDATA)\DeadByDaylight\Saved\Config\WindowsNoEditor\"
 $epic = "$($ENV:LOCALAPPDATA)\DeadByDaylight\Saved\Config\EGS\"
 $microsoft = "$($ENV:LOCALAPPDATA)\DeadByDaylight\Saved\Config\WinGDK\"
+$dirs = @($steam,$epic,$microsoft)
+$vers = @("Steam", "Epic Games", "Windows Store/Game Pass")
 
-Write-Output "Note: This will not guarantee the game will run at 120 fps, it simply unlocks the game to run UP TO that frame rate. The game is also capped at 120 frames per second, so that is the maximum achievable frame rate."
+Write-Host "Note: This will not guarantee the game will run at 120 fps, it simply unlocks the game to run UP TO that frame rate. `nThe game is capped at 120 frames per second, so that is the maximum achievable frame rate. `nThis script requires the values in the config files to be set to their default." -ForegroundColor Yellow -BackgroundColor Black
+Read-Host -Prompt "Press enter to patch any version of the game currently installed."
 
-if(Test-Path -Path $steam)
-{
-    Write-Output "Found Steam Version"
-    (Get-Content -Path "$($steam)\GameUserSettings.ini" -Raw) -replace "FPSLimit=30", "FPSLimit=120" | Set-Content -Path "$($steam)\GameUserSettings.ini"
-    (Get-Content -Path "$($steam)\GameUserSettings.ini" -Raw) -replace "bUseVSync=True", "bUseVSync=False" | Set-Content -Path "$($steam)\GameUserSettings.ini"
-    "" |Add-Content -Path "$($steam)\Engine.ini"
-    "[/script/engine.engine]" | Add-Content -Path "$($steam)\Engine.ini"
-    "bSmoothFrameRate=false" | Add-Content -Path "$($steam)\Engine.ini"
-    Write-Output "Patched Steam Version"
+For ($i = 0; $i -lt 3; $i++) {
+    if(Test-Path -Path $dirs[$i])
+    {
+        Write-Host "Found files for the $($vers[$i]) version." -ForegroundColor Yellow -BackgroundColor Black
+        (Get-Content -Path "$($dirs[$i])\GameUserSettings.ini" -Raw) -replace "FPSLimit=30", "FPSLimit=120" | Set-Content -Path "$($dirs[$i])\GameUserSettings.ini"
+        (Get-Content -Path "$($dirs[$i])\GameUserSettings.ini" -Raw) -replace "bUseVSync=True", "bUseVSync=False" | Set-Content -Path "$($dirs[$i])\GameUserSettings.ini"
+        "" |Add-Content -Path "$($dirs[$i])\Engine.ini"
+        "[/script/engine.engine]" | Add-Content -Path "$($dirs[$i])\Engine.ini"
+        "bSmoothFrameRate=false" | Add-Content -Path "$($dirs[$i])\Engine.ini"
+        Write-Host "All files for the $($vers[$i]) version have been patched" -ForegroundColor Yellow -BackgroundColor Black
+        cmd /c pause
+    }
+    else{
+        Write-Host "Did not find any files for the $($vers[$i]) version. Are you sure this version of the game is installed? You can ignore this message if you do not own this version." -ForegroundColor Yellow -BackgroundColor Black
+        cmd /c pause
+    }
 }
-if(Test-Path -Path $epic)
-{
-    Write-Output "Found Epic Games Version"
-    (Get-Content -Path "$($epic)\GameUserSettings.ini" -Raw) -replace "FPSLimit=30", "FPSLimit=120" | Set-Content -Path "$($epic)\GameUserSettings.ini"
-    (Get-Content -Path "$($epic)\GameUserSettings.ini" -Raw) -replace "bUseVSync=True", "bUseVSync=False" | Set-Content -Path "$($epic)\GameUserSettings.ini"
-    "" |Add-Content -Path "$($steam)\Engine.ini"
-    "[/script/engine.engine]" | Add-Content -Path "$($epic)\Engine.ini"
-    "bSmoothFrameRate=false" | Add-Content -Path "$($epic)\Engine.ini"
-    Write-Output "Patched Epic Games Version"
-}
-if(Test-Path -Path $microsoft)
-{
-    Write-Output "Found Windows Store/Game Pass Version"
-    (Get-Content -Path "$($microsoft)\GameUserSettings.ini" -Raw) -replace "FPSLimit=30", "FPSLimit=120" | Set-Content -Path "$($microsoft)\GameUserSettings.ini"
-    (Get-Content -Path "$($microsoft)\GameUserSettings.ini" -Raw) -replace "bUseVSync=True", "bUseVSync=False" | Set-Content -Path "$($microsoft)\GameUserSettings.ini"
-    "" |Add-Content -Path "$($steam)\Engine.ini"
-    "[/script/engine.engine]" | Add-Content -Path "$($microsoft)\Engine.ini"
-    "bSmoothFrameRate=false" | Add-Content -Path "$($microsoft)\Engine.ini"
-    Write-Output "Patched Found Windows Store/Game Pass Version"
-}
+
+Write-Host""
+Write-Host "All versions installed (if any) have been patched. `nPlease test to verify the game's framerate is unlocked to 120 FPS." -ForegroundColor Yellow -BackgroundColor Black
+cmd /c pause
